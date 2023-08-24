95db4a8fbf972969d8a323fc27048d9ca382dfd0
anfaly <142661981+anfaly@users.noreply.github.com>
2023-08-24 10:58:02 +0300

    anfal

---
+++ main1.py
@@ -0,0 +1,26 @@
+            # Change the following print statement so that it uses the addition operator + and prints out 8
+
+# Write a print statement that uses the division operator / 
+
+# Add a print statement that uses multiplication *
+
+# Create a variable called `length` and assign a value to it
+
+# Create a variable called    `width` and assign a value to it
+
+# Create a variable called `area` and assign to it the multiplication of `length` and `width`
+
+# Print `ANFAL` in the following: "The result is " followed by the `ANFAL` variable
+
+
+1# BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
+print("ANFAL ALMUTAIRI")
+X = 4
+Y = 4
+print(X+Y)
+print(X/Y)
+print(X*Y)
+lENGTH = 5
+WIDTH = 3
+AREA = (WIDTH * lENGTH)
+print(F"THE RESULT IS {AREA}")
