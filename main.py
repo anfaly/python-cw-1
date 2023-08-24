9bbc50b4b0586eb8f778631b0383e873edfd6f20
anfaly <142661981+anfaly@users.noreply.github.com>
2023-08-24 08:18:26 +0300

    done

--- main.py
+++ main.py
@@ -14,10 +14,17 @@
 
 
 1# BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
-NAME = "ANFAL" 
-ARYA = 4
-
-print("ARYA + ARYA")
-
-
-
+print("ANFAL ALMUTAIRI")
+X = 4
+Y = 4
+print(X+Y)
+print(X\y)
+print(X*Y)
+lENGTH = 5
+WIDTH = 3
+AREA = (WIDTH * lENGTH)
+print(F"THE RESULT IS {AREA}")
+print("6*3")
+lENGTH = 5
+WIDTH = 3
+AREA = 15
